b044c2ac36757e0a4eb410025e599b9314e9c8c0
Kathrine McAulay <kathrine.mcaulay@gmail.com>
2020-10-14 15:00:13 -0700

    automated mothur installation, this closes #9

---
+++ code/get_mothur.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Author: Kathrine McAulay
+# Inputs:
+# Outputs: Downlaods mothur into code/
+
+# This downloads and extracts mothur v1.44.3 into code/
+
+
+wget -P code/ -nc https://github.com/mothur/mothur/releases/download/v1.44.3/Mothur.OSX-10.14.zip
+unzip -d code/ code/Mothur.OSX-10.14.zip

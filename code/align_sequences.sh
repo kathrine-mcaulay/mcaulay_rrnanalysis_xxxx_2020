9af419e8b74832ee53fabcf85de996e7c8c603d3
Kathrine McAulay <kathrine.mcaulay@gmail.com>
2020-10-15 08:31:30 -0700

    updated inputs and outputs for clarification

--- code/align_sequences.sh
+++ code/align_sequences.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Author: Kathrine McAulay
-# Inputs: None
-# Outputs: Mothur executes align.seqs command with specified files
+# Inputs: data/raw/rrnDB-5.6_16S_rRNA.fasta
+#         data/references/silva_seed/silva.seed_v138.align
+# Outputs: data/raw/rrnDB-5.6_16S_rRNA.align
+# We need to include flip=T to make sure all sequences are in the same direction
 
-code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.6_16S_rRNA.fasta, reference=data/references/silva_seed/silva.seed_v138.align, flip=T)'
+code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.6_16S_rRNA.fasta,
+reference=data/references/silva_seed/silva.seed_v138.align, flip=T)'
